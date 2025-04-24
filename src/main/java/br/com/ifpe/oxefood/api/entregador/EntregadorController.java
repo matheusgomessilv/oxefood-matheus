9f010aaf52f8f56a140610c7c7f1fbaf84d4c94c
Matheus <mg93140@gmail.com>
2025-04-23 21:33:46 -0300

    terceiro commit

---
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -0,0 +1,44 @@
+package br.com.ifpe.oxefood.api.entregador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
+
+@RestController //Faz a classe ser um controller
+@RequestMapping("/api/entregador")
+@CrossOrigin //Utilizada para o controller receber requisições do React
+public class EntregadorController {
+      @Autowired //Instanciar no cliente service
+   private EntregadorService entregadorService;
+
+   @PostMapping //Especificar que essa função vai receber requisições do tipo Post
+   public ResponseEntity<Entregador> save(@RequestBody EntregadorRequest request) {
+
+       Entregador entregador = entregadorService.save(request.build());
+       return new ResponseEntity<Entregador>(entregador, HttpStatus.CREATED);
+   }
+
+  @GetMapping
+    public List<Entregador> listarTodos() {
+        return entregadorService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Entregador obterPorID(@PathVariable Long id) {
+        return entregadorService.obterPorID(id);
+    }
+
+
+}
---
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -0,0 +1,45 @@
+package br.com.ifpe.oxefood.api.produto;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ifpe.oxefood.modelo.produto.Produto;
+import br.com.ifpe.oxefood.modelo.produto.ProdutoService;
+
+
+@RestController //Faz a classe ser um controller
+@RequestMapping("/api/produto")
+@CrossOrigin //Utilizada para o controller receber requisições do React
+public class ProdutoController {
+       @Autowired //Instanciar no cliente service
+   private ProdutoService produtoService;
+
+   @PostMapping //Especificar que essa função vai receber requisições do tipo Post
+   public ResponseEntity<Produto> save(@RequestBody ProdutoRequest request) {
+
+       Produto produto = produtoService.save(request.build());
+       return new ResponseEntity<Produto>(produto, HttpStatus.CREATED);
+   }
+
+  @GetMapping
+    public List<Produto> listarTodos() {
+        return produtoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Produto obterPorID(@PathVariable Long id) {
+        return produtoService.obterPorID(id);
+    }
+
+
+}
