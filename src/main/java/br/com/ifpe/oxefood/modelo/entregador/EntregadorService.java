acaff9215b5cc97563f1c3f9fda07ffa1224bb2e
Matheus <mg93140@gmail.com>
2025-04-30 21:19:04 -0300

    quinto commit

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -30,7 +31,38 @@ public class EntregadorService {
 
         return repository.findById(id).get();
     }
+@Transactional
+   public void update(Long id, Entregador entregadorAlterado) {
 
+      Entregador entregador = repository.findById(id).get();
+      entregador.setNome(entregadorAlterado.getNome());
+      entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+      entregador.setCpf(entregadorAlterado.getCpf());
+      entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+      entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+      entregador.setRg(entregadorAlterado.getRg()); 
+      entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+      entregador.setValorFrete(entregadorAlterado.getValorFrete());
+      entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+      entregador.setEnderecoComplemento(entregadorAlterado.getEnderecoComplemento());
+      entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
+      entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+      entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+      entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+      entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf()); 
+      entregador.setAtivo(entregadorAlterado.getAtivo());
+
+      repository.save(entregador);
+  }
+
+  @Transactional
+  public void delete(Long id) {
+
+     Entregador entregador = repository.findById(id).get();
+      entregador.setHabilitado(Boolean.FALSE);
+
+      repository.save(entregador);
+  }
 
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -30,6 +31,27 @@ public class ProdutoService {
         return repository.findById(id).get();
     }
 
+@Transactional
+   public void update(Long id, Produto produtoAlterado) {
+
+      Produto produto = repository.findById(id).get();
+      produto.setCodigo(produtoAlterado.getCodigo());
+      produto.setTitulo(produtoAlterado.getTitulo());
+      produto.setDescricao(produtoAlterado.getDescricao());
+      produto.setValorUnitario(produtoAlterado.getValorUnitario());
+      produto.setTempoEntregaMinimo(produtoAlterado.getTempoEntregaMinimo());
+      produto.setTempoEntregaMaximo(produtoAlterado.getTempoEntregaMaximo());
+      repository.save(produto);
+  }
+
+  @Transactional
+   public void delete(Long id) {
+
+       Produto produto = repository.findById(id).get();
+       produto.setHabilitado(Boolean.FALSE);
+
+       repository.save(produto);
+   }
 
 
 }
